3501342cb82a18c972497a454dde64f39c762539
5aloh <salomon.hernandez@parkerandace.com>
2023-04-11 15:29:38 -0700

    Modular baseClass for API

--- src/test/java/comtest/api/LogInApi.java
+++ src/test/java/comtest/api/LogInApi.java
@@ -7,30 +7,45 @@ import org.testng.annotations.BeforeMethod;
 
 public class LogInApi {
     @BeforeMethod(alwaysRun = true)
+    // Method to send login request and return response
+    public Response sendLoginRequest(String username, String password) {
+        // Set base URL
+        RestAssured.baseURI = "https://dev.parkerandace.com";
+
+        // Set request body with username and password
+        String requestBody = "{ \"userName\": \"" + username + "\", \"password\": \"" + password + "\" }";
+
+        // Send POST request to generate Bearer token
+        Response response = RestAssured.given()
+                .contentType(ContentType.JSON)
+                .body(requestBody)
+                .post("/api/v1/application/login");
+
+        return response;
+    }
+
+    // Method to extract jwtToken from response
+    public String extractJwtToken(Response response) {
+        // Check if response body is not null or empty
+        String responseBody = response.getBody().asString();
+        System.out.println(responseBody);
+
+        // Extract the jwtToken from the response
+        String jwtToken = response.jsonPath().getString("jwtToken");
+
+        // Print the jwtToken to the console
+        System.out.println("Bearer Token: " + jwtToken);
+
+        return jwtToken;
+    }
+
     public String bearerGenerator() {
+        // Send login request and get response
+        Response response = sendLoginRequest("parkertestingace+505668518@gmail.com", "1234asdF@");
 
-            // Set base URL
-            RestAssured.baseURI = "https://dev.parkerandace.com";
+        // Extract jwtToken from response
+        String jwtToken = extractJwtToken(response);
 
-            // Set request body with username and password
-            String requestBody = "{ \"userName\": \"parkertestingace+505668518@gmail.com\", \"password\": \"1234asdF@\" }";
-
-            // Send POST request to generate Bearer token
-            Response response = RestAssured.given()
-                    .contentType(ContentType.JSON)
-                    .body(requestBody)
-                    .post("/api/v1/application/login");
-
-            // Check if response body is not null or empty
-            String responseBody = response.getBody().asString();
-            System.out.println(responseBody);
-
-            // Extract the jwtToken from the response
-            String jwtToken = response.jsonPath().getString("jwtToken");
-
-            // Print the jwtToken to the console
-            System.out.println("Bearer Token: " + jwtToken);
-                    return jwtToken;
-        }
-
+        return jwtToken;
+    }
 }
